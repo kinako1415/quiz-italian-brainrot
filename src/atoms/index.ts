2bd6e8aab9fad67197a9300e30eaba3f0d31bcd3
kinako1415 <Owata09@outlook.jp>
2025-05-20 22:04:55 +0900

    wip

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,7 +1,14 @@
 import { atom } from "jotai";
 
 export const currentQuestionIndexAtom = atom(0);
-export const questionsAtom = atom<{ sound: string; correctAnswer: string; incorrectAnswers: string[]; questionText: string }[]>([]);
+export const questionsAtom = atom<
+  {
+    sound: string;
+    correctAnswer: string;
+    incorrectAnswers: string[];
+    questionText: string;
+  }[]
+>([]);
 export const selectedAnswerAtom = atom<string | null>(null);
 export const gameStatusAtom = atom<"start" | "ready" | "playing" | "finished">(
   "ready"
