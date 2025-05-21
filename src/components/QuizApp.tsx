24b4159243c7b7cc0e0c6fe79d8c088b71d5cead
kinako1415 <Owata09@outlook.jp>
2025-05-21 10:31:13 +0900

    結果画面のスタイル修正

--- src/components/QuizApp.tsx
+++ src/components/QuizApp.tsx
@@ -164,22 +164,34 @@ const QuizApp = ({ setStarted }: { setStarted: (value: boolean) => void }) => {
 
   if (gameStatus === "finished") {
     return (
-      <div className="text-white text-center space-y-6 p-8 bg-gradient-to-br from-purple-800 to-gray-900 rounded-lg shadow-2xl transform scale-105 animate-fade-in">
-        <h2 className="text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-400 drop-shadow-lg">
-          結果発表
-        </h2>
-        <p className="text-2xl font-semibold">
-          正解数: <span className="text-green-400">{correctAnswers}</span> /{" "}
-          {questions.length}
-        </p>
-        <p className="text-2xl font-semibold">
-          合計時間:{" "}
-          <span className="text-yellow-400">
-            {(totalElapsedTime / 1000).toFixed(2)} 秒
-          </span>
-        </p>
+      <div
+        className="text-white text-center space-y-6 p-8 bg-gradient-to-br  shadow-2xl transform scale-105 animate-fade-in"
+        style={{
+          background: "rgba(255, 255, 255, 0.05)",
+          boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+          backdropFilter: "blur(6.5px)",
+          WebkitBackdropFilter: "blur(6.5px)",
+          borderRadius: "10px",
+          border: "1px solid rgba(255, 255, 255, 0.18)",
+        }}
+      >
+        <div className="flex flex-col items-center gap-4">
+          <h2 className="text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-400 drop-shadow-lg">
+            結果発表
+          </h2>
+          <p className="text-1xl font-semibold">
+            正解数: <span className="text-green-400">{correctAnswers}</span> /{" "}
+            {questions.length}
+          </p>
+          <p className="text-1xl font-semibold">
+            合計時間:{" "}
+            <span className="text-yellow-400">
+              {(totalElapsedTime / 1000).toFixed(2)} 秒
+            </span>
+          </p>
+        </div>
         <button
-          className="mt-6 px-8 py-3 bg-gradient-to-r from-purple-400 to-pink-500 text-white font-bold rounded-full shadow-lg hover:from-pink-500 hover:to-purple-400 transition-all duration-300 transform hover:scale-110"
+          className="px-8 py-3 bg-gradient-to-r from-purple-400 to-pink-500 text-white font-bold rounded-full shadow-lg transition-all duration-300 transform hover:scale-104"
           onClick={() => {
             setStarted(false);
             setCurrentQuestionIndex(0);
